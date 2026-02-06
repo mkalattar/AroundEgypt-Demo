8d42002aa054460dce8a5571c1248333d8becb51
Mohamed K. Attar <AttarMohamedK@gmail.com>
2026-02-06 13:59:27 +0200

    initial commit

---
+++ AroundEgypt-Demo/AroundEgypt_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundEgypt_DemoApp.swift
+//  AroundEgypt-Demo
+//
+//  Created by Mohamed K Attar on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AroundEgypt_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
